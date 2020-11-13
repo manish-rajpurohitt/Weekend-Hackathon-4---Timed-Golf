da14f76b436ede47979eea55b543f1ab20e24c39
Manish <manishkumar9866@gmail.com>
2020-11-13 20:01:46 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,68 @@ import "../styles/App.css";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
-  }
-  componentDidMount() {
-    
-  }
+    this.state = { time: 0, x: 0, y: 0,renderr:false };
+    this.handleStart=()=>{
+      this.start = Date.now();
+      this.interval = setInterval(() => this.setState({ time: Math.floor((Date.now() - this.start)/1000) }), 1000);
+      this.setState({renderr:true});
 
-  componentWillUnmount() {
+    }
+    this.handleKeys=(event)=>{
+      if(!((Number(this.state.x) === 255 || Number(this.state.y) === 250))||!((Number(this.state.x) === 250 || Number(this.state.y) === 255))){
+      if(event.key==="ArrowRight"){
+        let xCopy = this.state.x;
+        xCopy += 5;
+        this.setState({x:xCopy});
+        return;
+      }if(event.key==="ArrowDown"){
+        let yCopy = this.state.y;
+        yCopy += 5;
+        this.setState({y:yCopy});
+        return;
+      }
+      if(event.key === "ArrowLeft"){
+        let xCopy = this.state.x;
+        xCopy -= 5;
+        this.setState({x:xCopy});
+        return;
+      }if(event.key === "ArrowUp"){
+        let yCopy = this.state.y;
+        yCopy -= 5;
+        this.setState({y:yCopy});
+        return;
+      }
+    }else{
+      clearInterval(this.interval);
+        return;
+    }
+      
+    }
     
+    
+    
+  }
+  
+  componentDidMount() {
+    document.addEventListener("keydown",()=>this.handleKeys(event));
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+    this.interval = setInterval(() => this.setState({ time: Math.floor((Date.now() - this.start)/1000) }), 1000);
+
+    document.removeEventListener("keydown");
   }
 
 
 
   render() {
+    
     return (
  <>
+ 
+    {this.state.renderr?<><div className="heading-timer">{this.state.time}</div>
+ <div className="ball" style={{left:`${this.state.x}px`,top:`${this.state.y}px`}} ></div>
+ <div className="hole"></div></> : <button className="start" onClick={()=>this.handleStart()}>start</button>}
 </>
     );
   }
