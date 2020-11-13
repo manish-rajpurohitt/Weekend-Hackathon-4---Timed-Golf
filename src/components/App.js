89445c191255a8052ba2df583e23a859fbbe2858
Manish Purohit <manishkumar9866@gmail.com>
2020-11-13 15:47:33 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,80 +1,62 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import "../styles/App.css";
-class Timer extends React.Component {
+class Timer extends Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
-    this.handleStart = () => {
-      this.start = Date.now();
-      this.interval = setInterval(
-        () =>
-          this.setState({ time: Math.floor((Date.now() - this.start) / 1000) }),
-        1000
-      );
-      document.addEventListener("keydown", () => this.handleKeys(event));
-    };
-    this.handleKeys = (event) => {
-      if (
-        !(Number(this.state.x) === 255 || Number(this.state.y) === 250) ||
-        !(Number(this.state.x) === 250 || Number(this.state.y) === 255)
-      ) {
-        if (event.key === "ArrowRight") {
-          let xCopy = this.state.x;
-          xCopy += 5;
-          this.setState({ x: xCopy });
-          return;
-        }
-        if (event.key === "ArrowDown") {
-          let yCopy = this.state.y;
-          yCopy += 5;
-          this.setState({ y: yCopy });
-          return;
-        }
-        if (event.key === "ArrowLeft") {
-          let xCopy = this.state.x;
-          xCopy -= 5;
-          this.setState({ x: xCopy });
-          return;
-        }
-        if (event.key === "ArrowUp") {
-          let yCopy = this.state.y;
-          yCopy -= 5;
-          this.setState({ y: yCopy });
-          return;
-        }
-      } else {
-        clearInterval(this.interval);
-        return;
-      }
-    };
+    this.tid = 0;
+    this.state = { time: 0, x: 0, y: 0, isStarted: false };
+    this.moveTheBall = this.moveTheBall.bind(this);
+    this.startGame = this.startGame.bind(this);
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    document.addEventListener("keydown", this.moveTheBall, false);
+  }
+  componentDidUpdate() {
+    if (this.state.x === 250 && this.state.y === 250) {
+      clearInterval(this.tid);
+      document.removeEventListener("keydown", this.moveTheBall);
+    }
+  }
   componentWillUnmount() {
-    clearInterval(this.interval);
-    this.interval = setInterval(
-      () =>
-        this.setState({ time: Math.floor((Date.now() - this.start) / 1000) }),
-      1000
-    );
+    document.removeEventListener("keydown", this.moveTheBall);
+    clearInterval(this.tid);
+  }
 
-    document.removeEventListener("keydown");
+  moveTheBall(event) {
+    let { x, y } = this.state;
+    if (!this.state.isStarted) {
+      return;
+    }
+    if (event.key === "ArrowDown" || event.keyCode === 40) {
+      this.setState({ y: y + 5 });
+    } else if (event.key === "ArrowUp" || event.keyCode === 38) {
+      this.setState({ y: y - 5 });
+    } else if (event.key === "ArrowRight" || event.keyCode === 39) {
+      this.setState({ x: x + 5 });
+    } else if (event.key === "ArrowLeft" || event.keyCode === 37) {
+      this.setState({ x: x - 5 });
+    }
+  }
+
+  startGame() {
+    this.setState({ isStarted: true });
+    this.tid = setInterval(() => {
+      let t = this.state.time;
+      this.setState({ time: t + 1 });
+    }, 1000);
   }
 
   render() {
+    let ballPosition = { left: this.state.x + "px", top: this.state.y + "px" };
     return (
       <>
-        <div
-          className="ball"
-          style={{ left: `${this.state.x}px`, top: `${this.state.y}px` }}
-        ></div>
-        <div className="heading-timer">{this.state.time}</div>
-
-        <div className="hole"></div>
-
-        <button className="start" onClick={() => this.handleStart()}>
-          start
+        <div className="ball" style={ballPosition}></div>
+        <button className="start" onClick={this.startGame}>
+          Start
         </button>
+        <div className="hole"></div>
+        <div className="heading-timer">{this.state.time}</div>
       </>
     );
   }
